f7e77a89e9a7a913a4611105bbf3e95c08e243af
Maksim Kostromin <daggerok@gmail.com>
2019-06-26 15:14:16 +0300

    Update Globals.kt

--- buildSrc/src/main/java/Globals.kt
+++ buildSrc/src/main/java/Globals.kt
@@ -9,23 +9,23 @@ object Globals {
 
   val javaVersion = JavaVersion.VERSION_1_8
   const val vavrVersion = "0.10.0"
-  const val kotlinVersion = "1.3.31"
+  const val kotlinVersion = "1.3.40"
   const val lombokVersion = "1.18.8"
   const val junitVersion = "4.13-beta-3"
-  const val junitJupiterVersion = "5.5.0-RC1"
+  const val junitJupiterVersion = "5.5.0-RC2"
   const val r2dbcPostgresVersion = "1.0.0.M7"
   const val r2dbcSpringVersion = "0.1.0.M1"
   const val springBootVersion = "2.2.0.M3"
-  const val postgresVersion = "42.2.5"
+  const val postgresVersion = "42.2.6"
 
   object Gradle {
-    const val wrapperVersion = "5.5-rc-2"
+    const val wrapperVersion = "5.5-rc-4"
 
     object Plugin {
       const val lombokVersion = "3.1.0"
       const val versionsVersion = "0.21.0"
       const val flywayVersion = "6.0.0-beta2"
-      const val dependencyManagementVersion = "1.0.7.RELEASE"
+      const val dependencyManagementVersion = "1.0.8.RELEASE"
     }
   }
 }
